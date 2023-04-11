92b839144f73616fd285659de411a4428f85a950
Shishu <hung.lethanh@hotmail.com>
2023-04-11 13:27:37 +0200

    Revert "Ajout fichier widgets.py et model customer"
    This reverts commit c2b32b10e0e234a2dc53a7b8eeab2b49cb93016d.

--- src/backoffice/admin.py
+++ src/backoffice/admin.py
@@ -1,12 +1,3 @@
 from django.contrib import admin
-from backoffice.models import *
-from backoffice.widgets import PastCustomDatePickerWidget
 
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['lastname', 'firstname','birthday', 'weight']
-    formfield_overrides = {
-        models.DateField:{'widget': PastCustomDatePickerWidget}
-    }
-
-
-admin.site.register(Customer, CustomerAdmin)
+# Register your models here.
