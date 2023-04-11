70372cb359d12d026943354c3529164a5996a0c9
Shishu <hung.lethanh@hotmail.com>
2023-04-11 13:28:28 +0200

    Revert "Revert "Ajout fichier widgets.py et model customer""
    This reverts commit 92b839144f73616fd285659de411a4428f85a950.

--- src/backoffice/models.py
+++ src/backoffice/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import date
+from django.utils import timezone
 
-# Create your models here.
+#Cette class définit les abonnés
+class Customer(models.Model):
+    lastname = models.fields.CharField(blank=True, null=True, max_length=50)
+    firstname = models.fields.CharField(blank=True, null=True, max_length=50)
+    birthday = models.DateField(null=True, blank=True)
+    weight = models.FloatField(blank=True, null=True)
+
+    def __str__(self):
+        return f'{self.lastname} {self.firstname}'
