6eee1419221b1ccfa7923903980f4674787af907
Shishu <hung.lethanh@hotmail.com>
2023-04-15 13:18:27 +0200

    Configure Admin

--- src/backoffice/admin.py
+++ src/backoffice/admin.py
@@ -3,8 +3,49 @@ from backoffice.models import *
 from backoffice.widgets import PastCustomDatePickerWidget
 
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = [f.name for f in Customer._meta.fields]
-    #list_display_links = ['username']
+    fields = ['last_name',
+              'first_name',
+              'username',
+              'email',
+              'phone',
+              'address',
+              'address_num',
+              'address_box',
+              'address_zip_code',
+              'address_city',
+              'address_country',
+              'is_active',
+              'is_staff',
+              'is_superuser',
+              'date_joined',
+              'last_login',
+              'groups',
+              'user_permissions',
+              ]
+
+    #list_display = [f.name for f in Customer._meta.fields]
+    list_display = ['civility',
+                    'last_name',
+                    'first_name',
+                    'username',
+                    'email',
+                    'phone',
+                    'address',
+                    'address_num',
+                    'address_box',
+                    'address_zip_code',
+                    'address_city',
+                    'address_country',
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    ]
+    list_display_links = ['email']
+
+    list_filter = ['is_active','is_staff', 'is_superuser']
+
+    search_fields = ['last_name', 'first_name']
+
     formfield_overrides = {
         models.DateField:{'widget': PastCustomDatePickerWidget}
     }
